52f7a61ac871bb4dbe23e0b5cca97aaab972e500
moad6127 <moad6127@gmail.com>
2023-12-19 18:47:31 +0900

    Feat : TopDownRPG
    Character와 Enemy의 블루프린트 클래스 만들기 +
    BaseCharacter에 Weapon장착을 위한 SkeletalMeshComponent생성후 소켓에 장착시키기

--- Aura/Source/Aura/Private/Character/TopDownRPGCharacterBase.cpp
+++ Aura/Source/Aura/Private/Character/TopDownRPGCharacterBase.cpp
@@ -6,6 +6,9 @@ ATopDownRPGCharacterBase::ATopDownRPGCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ATopDownRPGCharacterBase::BeginPlay()
