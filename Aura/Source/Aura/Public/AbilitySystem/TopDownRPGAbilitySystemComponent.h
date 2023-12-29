2a822abaafe9cd4c80741209121267cf3f4dd1b2
moad6127 <moad6127@gmail.com>
2023-12-29 17:44:13 +0900

    Feat : TopDownRPG게임
    GAS를 위한 GamepalyAbilitySystemComponent와 AttributesSet클래스 생성하기
    +
    Build.cs파일의 PrivateDependency부분에 모듈 추가하기

---
+++ Aura/Source/Aura/Public/AbilitySystem/TopDownRPGAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "TopDownRPGAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UTopDownRPGAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
