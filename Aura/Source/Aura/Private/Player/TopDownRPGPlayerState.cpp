507a821e0c3a440189173d84ead2b677815beaff
moad6127 <moad6127@gmail.com>
2023-12-29 17:28:32 +0900

    Feat : TopDownRPG게임
    GameplayAbilitySystem 을 사용하기위한 PlayerState클래스 생성하기

---
+++ Aura/Source/Aura/Private/Player/TopDownRPGPlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/TopDownRPGPlayerState.h"
+
+ATopDownRPGPlayerState::ATopDownRPGPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
