29ba63d2088f3582afc7e489ebe4c7a97c091f30
moad6127 <moad6127@gmail.com>
2024-02-06 18:35:04 +0900

    Feat : TopDownRPG게임
    Modifire Magnitude Calculation(MMC)를 사용해서 MaxHealth와 MaxMana의 값을 Override하기
    +
    (Attributes값과 PlayerLevel값 모두 사용하기위해 MMC를 사용한다)

---
+++ Aura/Source/Aura/Private/AbilitySystem/MMC/MMC_MaxMana.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/MMC/MMC_MaxMana.h"
+#include "AbilitySystem/TopDownRPGAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UTopDownRPGAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntelligenceDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(IntelligenceDef);
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	//Gether tags form source and target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Intelligence = 0.f;
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, EvaluationParameters, Intelligence);
+	Intelligence = FMath::Max<float>(Intelligence, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 50.f + 25.f * Intelligence + 15.f * PlayerLevel;
+}
