0720ef213029b94ad107bfece76e930e0a1933bb
moad6127 <moad6127@gmail.com>
2024-02-14 17:28:11 +0900

    Feat : TopDownRPG게임
    AttributeMenuWidgetController를 TopDownRPGWIdgetController를 부모로 해서 생성하기

---
+++ Aura/Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/TopDownRPGWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UTopDownRPGWidgetController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BroadcastInitialValue() override;
+	virtual void BindCallbacksToDependencies() override;
+};
