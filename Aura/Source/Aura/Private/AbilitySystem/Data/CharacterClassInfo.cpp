f40637077ef5c2dbb99584d555fc351ea2d75a13
moad6127 <moad6127@gmail.com>
2024-03-08 14:13:21 +0900

    Feat : TopDownRPG게임
    캐릭터들의 원소술사, 도적, 전사같은 클래스를 만들기위해 DataAsset클래스를 새로 만들고 설정하기

---
+++ Aura/Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+
+}
