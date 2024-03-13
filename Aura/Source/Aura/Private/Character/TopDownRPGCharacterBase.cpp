eee325b309aee2679087d2ce2092ae9620c85052
moad6127 <moad6127@gmail.com>
2024-03-13 15:20:03 +0900

    Feat : TopDownRPG게임
    Enemy가 죽을때 DissolveEffect를 가지도록 설정하기

--- Aura/Source/Aura/Private/Character/TopDownRPGCharacterBase.cpp
+++ Aura/Source/Aura/Private/Character/TopDownRPGCharacterBase.cpp
@@ -53,6 +53,7 @@ void ATopDownRPGCharacterBase::MulticastHandleDeath_Implementation()
 	GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 
 	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	Dissolve();
 }
 
 void ATopDownRPGCharacterBase::BeginPlay()
@@ -96,6 +97,22 @@ void ATopDownRPGCharacterBase::AddCharacterAbilities()
 		return;
 	}
 	TopDownASC->AddCharacterAbilities(StartupAbilities);
+}
+
+void ATopDownRPGCharacterBase::Dissolve()
+{
+	if (IsValid(DissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(DissolveMaterialInstance, this);
+		GetMesh()->SetMaterial(0, DynamicMatInst);
+		StartDissolveTimeline(DynamicMatInst);
+	}
+	if (IsValid(WeaponDissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(WeaponDissolveMaterialInstance, this);
+		Weapon->SetMaterial(0, DynamicMatInst);
+		StartWeaponDissolveTimeline(DynamicMatInst);
+	}
 }
 
 
