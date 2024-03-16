cb8fdb3947842253c46046061cc1be997be45eca
moad6127 <moad6127@gmail.com>
2024-03-16 17:08:43 +0900

    Feat : TopDownRPG게임
    Source의 ArmorPenetration를 캡처하기
    +
    Damage계산을 좀더 세분화 시켜서 적용시키도록 만들기
    (Target의 Armor 적용, Source의 ArmorPenetration적용후 데미지 산정)

--- Aura/Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Aura/Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -11,10 +11,12 @@
 struct TopDownRPGDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 	TopDownRPGDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, ArmorPenetration, Source, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, BlockChance, Target, false);
 	}
 };
@@ -29,6 +31,7 @@ UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorPenetrationDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -51,16 +54,30 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	// Get Damage set by caller Magnitude;
 	float Damage = Spec.GetSetByCallerMagnitude(FTopDownRPGGameplayTags::Get().Damage);
 
-	//Capture BlockChance on Target, and determine if ther was a successful Block
-
+	//Blockchance를 캡처한후 Block됬는지를 판단한다
 	float TargetBlockChance = 0.f;
-
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluationParameters, TargetBlockChance);
 	TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.f);
+
 	bool bBlocking = FMath::RandRange(1, 100) < TargetBlockChance;
 
-	//If Block, half damage
+	//블록되면 Damage를 /2로 만든다
 	Damage = bBlocking ? Damage / 2.f : Damage;
+
+	// Target의 Armor과 Source의 ArmorPenetration를 캡처한다.
+	float TargetArmor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, TargetArmor);
+	TargetArmor = FMath::Max<float>(TargetArmor, 0.f);
+
+	float SourceArmorPenetration = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenetrationDef, EvaluationParameters, SourceArmorPenetration);
+	SourceArmorPenetration = FMath::Max<float>(SourceArmorPenetration, 0.f);
+
+	//Source의 방어관통력을 Target의 Armor에 적용시킨다(백분율로 적용시킴)
+	const float EffectiveArmor = TargetArmor *= (100 - SourceArmorPenetration * 0.25f) / 100.f;
+
+	// 적용된Target의 Armor에 따라서 Damage를 감소시킨다.
+	Damage *= (100 - EffectiveArmor * 0.3f) / 100.f;
 
 	const FGameplayModifierEvaluatedData EvaluatedData(
 		UTopDownRPGAttributeSet::GetIncomingDamageAttribute(),
