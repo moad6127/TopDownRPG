9ebfae06fc7bb35ff2c3012862572fae70806ff2
moad6127 <moad6127@gmail.com>
2024-03-18 15:41:23 +0900

    Feat : TopDownRPG게임
    CriticalChance 와 Damage , Resistance를 클래스에서 캡처한후 적용시키기
    +
    Resistance계수를 레벨에 따라서 변하도록 CT_DamageCurve에 Curve추가하기

--- Aura/Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Aura/Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -16,11 +16,17 @@ struct TopDownRPGDamageStatics
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitChance);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitDamage);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitResistance);
 	TopDownRPGDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, Armor, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, ArmorPenetration, Source, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, BlockChance, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, CriticalHitChance, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, CriticalHitDamage, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UTopDownRPGAttributeSet, CriticalHitResistance, Target, false);
 	}
 };
 
@@ -35,6 +41,9 @@ UExecCalc_Damage::UExecCalc_Damage()
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorPenetrationDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CriticalHitChanceDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CriticalHitDamageDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CriticalHitResistanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -81,6 +90,7 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const UCharacterClassInfo* CharacterClassInfo = UTopDownRPGAbilitySystemLibrary::GetCharacterClassInfo(SourceAvatar);
 	const FRealCurve* ArmorPenetrationCurve = CharacterClassInfo->DamageCalculationCoefficients->FindCurve(FName("ArmorPenetration"), FString());
 	const float ArmorPenetrationCoefficients = ArmorPenetrationCurve->Eval(SourceCombatInterface->GetPlayerLevel());
+	
 	//Source의 방어관통력을 Target의 Armor에 적용시킨다(백분율로 적용시킴)
 	const float EffectiveArmor = TargetArmor * (100 - SourceArmorPenetration * ArmorPenetrationCoefficients) / 100.f;
 
@@ -88,6 +98,29 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const float EffectiveArmorCoefficients = EffectiveArmorCurve->Eval(TargetCombatInterface->GetPlayerLevel());
 	// 적용된Target의 Armor에 따라서 Damage를 감소시킨다.
 	Damage *= (100 - EffectiveArmor * EffectiveArmorCoefficients) / 100.f;
+
+	//Critical선별하기
+	float SourceCriticalChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CriticalHitChanceDef, EvaluationParameters, SourceCriticalChance);
+	SourceCriticalChance = FMath::Max<float>(SourceCriticalChance, 0.f);
+
+	//Critical추가 Damage
+	float SourceCriticalDamage = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CriticalHitDamageDef, EvaluationParameters, SourceCriticalDamage);
+	SourceCriticalDamage = FMath::Max<float>(SourceCriticalDamage, 0.f);
+
+	//CriticalResistance발동시 Damage는 원상복구
+	float TargetCriticalResistance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CriticalHitResistanceDef, EvaluationParameters, TargetCriticalResistance);
+	TargetCriticalResistance = FMath::Max<float>(TargetCriticalResistance, 0.f);
+
+	const FRealCurve* CriticalResistanceCurve = CharacterClassInfo->DamageCalculationCoefficients->FindCurve(FName("CriticalHitResistance"), FString());
+	const float CriticalHitResistanceCoefficients = CriticalResistanceCurve->Eval(TargetCombatInterface->GetPlayerLevel());
+
+	const float EffectiveCriticalHitChance = SourceCriticalChance - TargetCriticalResistance * CriticalHitResistanceCoefficients;
+	const bool bCritical = FMath::RandRange(1, 100) < EffectiveCriticalHitChance;
+
+	Damage = bCritical ? Damage * (2.f + (SourceCriticalDamage * 100) / 100.f) : Damage;
 
 	const FGameplayModifierEvaluatedData EvaluatedData(
 		UTopDownRPGAttributeSet::GetIncomingDamageAttribute(),
