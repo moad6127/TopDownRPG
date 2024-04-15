955814a8a556d1581d983f7d47218462f4f51e14
moad6127 <moad6127@gmail.com>
2024-04-15 16:20:45 +0900

    Feat : TopDownRPG게임
    멀티플레이를 진행할시 생기는 버그 수정하기
    (Proejctile클래스에서 논리 오류가 발생함)

--- Aura/Source/Aura/Private/Actor/TopDownRPGProjectile.cpp
+++ Aura/Source/Aura/Private/Actor/TopDownRPGProjectile.cpp
@@ -61,7 +61,7 @@ void ATopDownRPGProjectile::Destroyed()
 
 void ATopDownRPGProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappingComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (DamageEffectSpecHandle.Data.IsValid() && DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
+	if (!DamageEffectSpecHandle.Data.IsValid() || DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
 	{
 		return;
 	}
