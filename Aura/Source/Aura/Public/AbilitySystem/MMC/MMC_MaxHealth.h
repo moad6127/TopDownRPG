39aaeb9e3217b99f8e514a5fc7ec586f0ebb8182
moad6127 <moad6127@gmail.com>
2024-05-15 19:58:01 +0900

    Feat : TopDownRPG게임
    버그 발견 : Multiplay로 play할때 Levelup하면 마지막 player만 SecondrayAttribute가 변함

--- Aura/Source/Aura/Public/AbilitySystem/MMC/MMC_MaxHealth.h
+++ Aura/Source/Aura/Public/AbilitySystem/MMC/MMC_MaxHealth.h
@@ -13,7 +13,6 @@ UCLASS()
 class AURA_API UMMC_MaxHealth : public UGameplayModMagnitudeCalculation
 {
 	GENERATED_BODY()
-public:
 	UMMC_MaxHealth();
 
 	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
