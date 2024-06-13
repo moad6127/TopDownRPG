6288fa999e6982d1d3976dbb5eae43ac5c755609
moad6127 <moad6127@gmail.com>
2024-06-13 16:33:49 +0900

    Feat : TopDownRPG게임
    Client가 Firebolt를 사용할때 움직임이 다른버그 수정
    (ReplicateMovememt를 True로 설정하기)
    +
    Client의 Firebolt가 벽에서 터질때 버그 수정

--- Aura/Source/Aura/Private/Actor/TopDownRPGProjectile.cpp
+++ Aura/Source/Aura/Private/Actor/TopDownRPGProjectile.cpp
@@ -15,6 +15,7 @@ ATopDownRPGProjectile::ATopDownRPGProjectile()
 	PrimaryActorTick.bCanEverTick = false;
 
 	bReplicates = true;
+	SetReplicateMovement(true);
 
 	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
 	SetRootComponent(Sphere);
@@ -70,6 +71,10 @@ void ATopDownRPGProjectile::OnHit()
 
 void ATopDownRPGProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappingComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (!IsValid(DamageEffectParams.SourceAbilitySystemComponent))
+	{
+		return;
+	}
 	AActor* SourceAvatarActor = DamageEffectParams.SourceAbilitySystemComponent->GetAvatarActor();
 	if (SourceAvatarActor == OtherActor)
 	{
