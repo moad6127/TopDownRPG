11d13064688d1b99acdd46b392dbf245cbccbee2
moad6127 <moad6127@gmail.com>
2024-07-03 15:22:19 +0900

    Feat : TopDownRPG게임
    ViewModel을 구성하기
    (
    LoadMap과 관련된 위젯 블루프린트들의 부모가 되는 새로운 위젯 블루프린트인 WBP_loadScreenBast를 LoadScreenWidet으로 파생해서 만들고 기존의 위젯블루프린트들의 부모를 바꾸기
    +
    LoadScreenHUD클래스에서 화면에 LoadScreen을 보이게 만들기
    +
    HUD클래스에서 MVVMModel을 NewObject로 생성하기
    +
    MVVM_LoadScreen을 파생해서 블루프린트 만들기
    [현재 버전인 5.1에서는 기본적으로 블루프린트를 못만들어서 UCLASS에서 BlueprintType, Blueprintable을 추가해서 생성했다.]
    
    위젯 블루프린트의 ViewModel을 사용해서 BP_LoadScreenViewModel을 얻도로 만들기
    )

--- Aura/Source/Aura/Public/UI/HUD/LoadScreenHUD.h
+++ Aura/Source/Aura/Public/UI/HUD/LoadScreenHUD.h
@@ -6,12 +6,30 @@
 #include "GameFramework/HUD.h"
 #include "LoadScreenHUD.generated.h"
 
+
 /**
  * 
  */
+class ULoadScreenWidget;
+class UMVVM_LoadScreen;
+
 UCLASS()
 class AURA_API ALoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
+public:
 	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ULoadScreenWidget> LoadScreenWidget;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadScreen> LoadScreenViewModelClass;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UMVVM_LoadScreen> LoadScreenViewModel;
+protected:
+	virtual void BeginPlay() override;
 };
--- Aura/Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Aura/Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -9,7 +9,7 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(BlueprintType, Blueprintable)
 class AURA_API UMVVM_LoadScreen : public UMVVMViewModelBase
 {
 	GENERATED_BODY()
