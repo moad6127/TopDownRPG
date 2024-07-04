7bd49e70454f55bd1eacda2f69184d90723e89ad
moad6127 <moad6127@gmail.com>
2024-07-04 15:13:48 +0900

    Feat : TopDownRPG게임
    LoadSlotViewModel클래스를 새롭게 만들고
    LoadSlot들의 Switcher들에게 각각의 ViewModel을 붙여주기
    (Switcher의 내부의 위젯에 각각의 ViewModel을 붙이기)

--- Aura/Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Aura/Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -9,9 +9,32 @@
 /**
  * 
  */
+class UMVVM_LoadSlot;
+
 UCLASS(BlueprintType, Blueprintable)
 class AURA_API UMVVM_LoadScreen : public UMVVMViewModelBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	void InitializeLoadSlots();
+
+	UFUNCTION(BlueprintPure)
+	UMVVM_LoadSlot* GetLoadSlotViewModelByIndex(int32 Index) const;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadSlot> LoadSlotViewModelClass;
+
+private:
+	UPROPERTY()
+	TMap<int32, UMVVM_LoadSlot*> LoadSlots;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_0;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_1;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_2;
 };
---
+++ Aura/Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "MVVM_LoadSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class AURA_API UMVVM_LoadSlot : public UMVVMViewModelBase
+{
+	GENERATED_BODY()
+	
+};
