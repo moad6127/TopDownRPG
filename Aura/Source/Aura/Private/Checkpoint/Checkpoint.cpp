e4967d9c4ec57742ac419c707e21fc642c43f150
moad6127 <moad6127@gmail.com>
2024-07-11 15:37:47 +0900

    Feat : TopDownRPG게임
    게임을 저장할 Checkpoint를 만들기
    (
    PlayerStart를 기반으로 Checkpoint클래스 만들기
    Mesh와 Sphere를 추가해서 Chareter가 겹치면 Checkpoint가 발동되도록 만들기
    +
    Checkpoint를 표시하기 위해 Player가 Checkpoint와 겹치면 Glow가 변하게 만들기
    )

---
+++ Aura/Source/Aura/Private/Checkpoint/Checkpoint.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "Checkpoint/Checkpoint.h"
+#include "Components/SphereComponent.h"
+
+
+
+ACheckpoint::ACheckpoint(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckpointMesh");
+	CheckpointMesh->SetupAttachment(GetRootComponent());
+	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CheckpointMesh->SetCollisionResponseToAllChannels(ECR_Block);
+}
+
+void ACheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappingComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag(FName("Player")))
+	{
+		HandleGlowEffects();
+	}
+}
+
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnSphereOverlap);
+}
+
+void ACheckpoint::HandleGlowEffects()
+{
+	Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	UMaterialInstanceDynamic* DynamicMaterialInstance = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+
+	CheckpointMesh->SetMaterial(0, DynamicMaterialInstance);
+
+	CheckpointReached(DynamicMaterialInstance);
+}
